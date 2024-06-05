a82713c3b6263bd101d77a4f6106a793a197e2dc
Clinton-G <166076564+Clinton-G@users.noreply.github.com>
2024-06-05 18:29:24 -0500

    Add files via upload

--- Project2.py
+++ Project2.py
@@ -1,7 +1,4 @@
-#   contacts needs name, phone mumber, and email address. contact deatils in the INNER disctionary, phone/email as OUTTER dictionary
-
-
-contact ={
+contacts = {
     'hashslinging@slasher.com': {'Name': 'Spongebob Squarepants', 'Phone': '999 111 1111'},
     'wumbology@wumbo.com': {'Name': 'Patrick Star', 'Phone': '999 222 2222'},
     'martialarts@karate.com': {'Name': 'Sandy Cheeks', 'Phone': '999 333 3333'},
@@ -9,110 +6,93 @@ contact ={
 }
 
 
-print('''
-Welcome to the Contact Management System!
---------------------
-       Menu:
---------------------
-1: Add New Contact
-2: Edit Existing Contact
-3: Delete a Contact
-4: Search Contacts
-5: Display all Contacts
-6: Export Contacts to .txt file
-7: Import Contacts from .txt file
-0: Quit
--------------------- ''')
-menu = (input("Enter Input: "))
+def add_contact():
+    email = input('Email: ')
+    name = input('Name: ')
+    phone = input('Phone # ')
+    contacts[email] = {'Name': name, 'Phone': phone}
+    print(name, 'Added Successfully!')
+
+
+def edit_contact():
+    email = input("Enter Contact's Email to Edit: ")
+    if email in contacts:
+        field = input('Enter Edit (Name/Phone): ')
+        new_value = input(f'Enter new {field}: ')
+        contacts[email][field] = new_value
+        print('Contact Updated. ')
+    else:
+        print('Contact Not Found, Please Try Again. ')
+
+
+def delete_contact():
+    email = input('Enter email of contact you wish to delete: ')
+    if email in contacts:
+        del contacts[email]
+        print('Contact deleted successfully!')
+    else:
+        print('Contact not found.')
+
+
+def search_contact():
+    email = input('Enter Email to Search: ')
+    if email in contacts:
+        print('- - - - - - - - - -')
+        print('Contact Details:')
+        print('Name:', contacts[email]['Name'])
+        print('Phone:', contacts[email]['Phone'])
+    else:
+        print('Contact Not Found, Please Try Again. ')
+
+
+def display_contacts():
+    print('Roladex Info:')
+    for email, details in contacts.items():
+        print('- - - - - - - - - -')
+        print('Email:', email)
+        print('Name:', details['Name'])
+        print('Phone:', details['Phone'])
+
+
+while True:
+    print('''
+    Welcome to the Contact Management System!
+    --------------------
+           Menu:
+    --------------------
+    1: Add New Contact
+    2: Edit Existing Contact
+    3: Delete a Contact
+    4: Search Contacts
+    5: Display all Contacts
+    0: Quit
+    -------------------- ''')
+    choice = input("Enter Input: ")
+
+    if choice == '1':
+        add_contact()
+        
+    elif choice == '2':
+        edit_contact()
+
+    elif choice == '3':
+        delete_contact()
+
+    elif choice == '4':
+        search_contact()
+
+    elif choice == '5':
+        display_contacts()
+
+    elif choice == '0':
+        print('Good Bye, Have a Good Day. ')
+        break
+    else:
+        print('Invalid Input. Please Try Again. ')
 
 
 
 
-# Menu Actions:
-# Implement the following actions in response to menu selections:
-#  1 Adding a new contact with all relevant details.
-#  2 Editing an existing contact's information (name, phone number, email, etc.).
-#  3 Deleting a contact by searching for their email.
-#  4 Searching for a contact by their unique email and displaying their details.
-#  5 Displaying a list of all contacts.
-#  6 Exporting contacts to a text file in a structured format.
-#  7 Importing contacts from a text file and adding them to the system.
-
-
-
-
-#   1 - Adding Contacts:
-while menu != 'stop adding':
-    if menu == '1':
-        email1 = input('Email: ')
-        name1 = input('Name: ')
-        phone1 = input('Phone: ')
-        print('----------')
-        print(name1,': Has Been Added!')
-        print('----------')
-        again = input('"yes" to add another, "stop adding" to end: ')
-        if again == 'stop adding':
-            print("Have a Nice Day!")
-            break
-
-
-
-
-#   2 - Editing Contact:
-    while menu != 'end edit':
-        if menu == '2':
-            edit = input("Enter Contact Email You Would Like to Edit: ")
-            if edit == 'Spongebob Squarepants':
-                sponge_edit = input('What Would You Like to Change? ')
-                if sponge_edit == ['Name']:
-                    print()
-                    break
-#   dictionary[existing_key] = new_value
-
-
-
-
-#   3 - Delete Contact:
-        while menu != 'stop delete':
-            if menu == '3':
-                contact_delete = input("Enter email of contact you wish to delete: ")
-                del contact[contact_delete]
-                print(contact_delete,': Has Been Deleted')
-                print("----------")
-                print("Would You Like to Delete Another? ")
-                print("----------")
-                more_del = input('"yes" to proceed, "stop delete" to end: ')
-                if more_del == 'stop delete':
-                    print("Have a Nice Day!")
-                    break
-                    # doesnt break for some reason :/
-
-
-
-
-#   4 - Search:
-            if menu == '4':
-                search = input("Enter Email to Search: ")
-                if search == 'hashslinging@slasher.com':
-                    print()
-                elif search == 'wumbology@wumbo.com':
-                    print()
-                elif search == 'martialarts@karate.com':
-                    print()
-                elif search == 'snailmail@gary.com':
-                    print()
-                else:
-                    print('Invald Email, Try Again')
-                    break
-
-
-
-#   5 - Display List:
-            else:
-                menu == '5'
-                contact_list = input('Press Enter to Display All Contacts: ')
-                print(contact)
-                break
 
 
 
